442830a790c8b993cfb3d224e1f3aeacd56470d2
Kenneth Mercer <kmercer5439@gmail.com>
2022-03-08 20:27:42 -0600

    set up server.js, building router

--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
+const express = require('express')
+const server = express()
+const postsRouter = require('./posts/posts-router')
+
+server.use(express.json())
+
+server.use('/api/posts', postsRouter)
+
+server.use('*', (req, res) => {
+  res.send('Server Running!')
+})
+
+module.exports = server
 // require your posts router and connect it here
